3b534e83f4266120e970bd0cde963dfef7b8928d
Samanta Karmaker <samonto0726@gmail.com>
2024-05-19 23:00:30 +0600

    Solved on 19/05/2024

---
+++ No104.java
@@ -0,0 +1,9 @@
+// maximum-depth-of-binary-tree
+
+class Solution {
+    int ans = 0;
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
