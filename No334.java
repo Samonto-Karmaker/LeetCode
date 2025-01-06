4ce36f08c14f56cfa68ee869a0964afd7cb5f9ec
Samanta Karmaker <samonto0726@gmail.com>
2025-01-06 23:15:00 +0600

    Solved on 06/01/2025

---
+++ No334.java
@@ -0,0 +1,18 @@
+// increasing-triplet-subsequence
+
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        if (nums.length < 3) return false;
+
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int n : nums) {
+            if (n <= min1) min1 = n;
+            else if (n <= min2) min2 = n;
+            else return true;
+        }
+
+        return false;
+    }
+}
